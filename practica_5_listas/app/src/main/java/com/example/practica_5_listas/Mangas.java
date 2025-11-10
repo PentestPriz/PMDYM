f54e84a823d65502ee04e21ef4e717e1cecefdf4
PentestPriz <hniktozzz@gmail.com>
2025-11-10 16:56:34 +0100

    Puntos 2.3 y 2.4

---
+++ practica_5_listas/app/src/main/java/com/example/practica_5_listas/Mangas.java
@@ -0,0 +1,18 @@
+package com.example.practica_5_listas;
+
+public class Mangas {
+
+    //Atributos de la clase
+    private String titulo;
+
+    private String imagen;
+
+    //Constructor de la clase
+    public Mangas(String titulo, String imagen) {
+
+            this.titulo = titulo;
+
+            this.imagen = imagen;
+
+    }//Cierre constructor
+}//Cierre clase
