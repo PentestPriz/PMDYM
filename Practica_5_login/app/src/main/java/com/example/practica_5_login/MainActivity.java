4ae19944b6599749814d4d78f8f486456c9afbca
PentestPriz <hniktozzz@gmail.com>
2025-11-10 15:27:11 +0100

    Punto 1.2

--- Practica_5_login/app/src/main/java/com/example/practica_5_login/MainActivity.java
+++ Practica_5_login/app/src/main/java/com/example/practica_5_login/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.practica_5_login;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText tCorreo;
+
+    private EditText tContraseña;
+
+    private Button bContinuar;
+
+    private Switch sRecordar;
+
+    private TextView tvMensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-}
+
+        //Encuentro los elementos por su id
+        tCorreo = findViewById(R.id.etCorreo);
+        tContraseña = findViewById(R.id.etContraseña);
+        bContinuar = findViewById(R.id.bContinuar);
+        sRecordar = findViewById(R.id.sRecordar);
+        tvMensaje = findViewById(R.id.tvMensaje);
+
+        //Código a ejecutar al clicar el botón continuar
+        bContinuar.setOnClickListener(v -> {
+
+            //Obtiene el contenido de los campos de texto
+            String correo = tCorreo.getText().toString();
+            String contraseña = tContraseña.getText().toString();
+
+            //Si los campos coinciden con los valores designados da un mensaje de bienvenida
+            if (correo.equals("correo@correo.com") && contraseña.equals("psw")) {
+                tvMensaje.setText("Contraseña y correo correctos, bienvenido.");
+            }//Cierre if
+        });
+    }//Cierre onCreate
+}//Cierre clase
