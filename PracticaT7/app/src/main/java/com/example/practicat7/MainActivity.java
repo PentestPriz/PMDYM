e6cb406db52d40c93d7f92fa6bb00109c580a232
PentestPriz <hniktozzz@gmail.com>
2025-11-15 22:20:42 +0100

    Realización del punto 1.2

--- PracticaT7/app/src/main/java/com/example/practicat7/MainActivity.java
+++ PracticaT7/app/src/main/java/com/example/practicat7/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.practicat7;
 
 import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Button;
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        //Declaración de los objetos a utilizar
+        //Declaración de los objetos a utilizar en el reproductor
         MediaPlayer mp = MediaPlayer.create(this, R.raw.suicide_tuesday);
         Button btnPlay = findViewById(R.id.btnPlay);
         Button btnPause = findViewById(R.id.btnPause);
@@ -36,6 +37,13 @@ public class MainActivity extends AppCompatActivity {
         TextView tvTiempoActual = findViewById(R.id.tvTiempoActual);
         TextView tvTiempoTotal = findViewById(R.id.tvTiempoTotal);
         TextView tvTitulo = findViewById(R.id.tvTitulo);
+
+        //Declaración de los objetos a utilizar en el soundpool (en realidad, solo botones)
+        Button btnSonido1 = findViewById(R.id.btnSonido1);
+        Button btnSonido2 = findViewById(R.id.btnSonido2);
+        Button btnSonido3 = findViewById(R.id.btnSonido3);
+        Button btnSonido4 = findViewById(R.id.btnSonido4);
+
 
         //Ponemos el título de la caja de texto
         tvTitulo.setText(getResources().getResourceEntryName(R.raw.suicide_tuesday));
@@ -93,6 +101,24 @@ public class MainActivity extends AppCompatActivity {
 
             @Override public void onStopTrackingTouch(SeekBar seekBar) {}
         });//Cierre setOnSeekBarChangeListener
+
+        //Reproductor de sonidos
+
+        //Declaración del soundpool
+        SoundPool sp = new SoundPool.Builder().setMaxStreams(10).build();
+
+        //Cargo los sonidos a reproducir en memoria
+        int sonido1 = sp.load(this, R.raw.bomb_has_been_planted, 1);
+        int sonido2 = sp.load(this, R.raw.cave, 1);
+        int sonido3 = sp.load(this, R.raw.classic_hurt, 1);
+        int sonido4 = sp.load(this, R.raw.round, 1);
+
+        //Definimos el accionamiento de los botones
+        btnSonido1.setOnClickListener(v -> sp.play(sonido1, 1, 1, 1, 0, 1));
+        btnSonido2.setOnClickListener(v -> sp.play(sonido2, 1, 1, 1, 0, 1));
+        btnSonido3.setOnClickListener(v -> sp.play(sonido3, 1, 1, 1, 0, 1));
+        btnSonido4.setOnClickListener(v -> sp.play(sonido4, 1, 1, 1, 0, 1));
+
 
     }//Cerre onCreate
 }//Cierre MainActivity
